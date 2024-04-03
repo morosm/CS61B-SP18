b06d33e69c812f1890fc86847999bb6da6aca1c1
plumm@foxmail.com <plumm@foxmail.com>
2024-04-03 18:14:17 +0800

    proj1A: add interface

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,51 @@
+public class LinkedListDeque<T> implements Deque<T>{
+
+    @Override
+    public void addFirst(T item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addFirst'");
+    }
+
+    @Override
+    public void addLast(T item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addLast'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public void printDeque() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printDeque'");
+    }
+
+    @Override
+    public T removeFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeFirst'");
+    }
+
+    @Override
+    public T removeLast() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeLast'");
+    }
+
+    @Override
+    public T get(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+}
