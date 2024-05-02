6c03c649da133afb45d8ab8e21f9d6ca87556d06
moros <plumm@foxmail.com>
2024-05-02 15:43:02 +0800

    lab5 finish

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,125 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+    /**
+     * Computes the width of row i for a size s hexagon.
+     * @param s The size of the hex.
+     * @param i The row number where i = 0 is the bottom row.
+     * @return
+     */
+    public static int hexRowWidth(int s, int i) {
+        int effectiveI = i;
+        if(i >= s){
+            effectiveI = s * 2 - i - 1;
+        }
+        return s + effectiveI * 2;
+    }
+
+    /**
+     * Computesrelative x coordinate of the leftmost tile in the ith
+     * row of a hexagon, assuming that the bottom row has an x-coordinate
+     * of zero. For example, if s = 3, and i = 2, this function
+     * returns -2, because the row 2 up from the bottom starts 2 to the left
+     * of the start position, e.g.
+     *   xxxx
+     *  xxxxxx
+     * xxxxxxxx
+     * xxxxxxxx <-- i = 2, starts 2 spots to the left of the bottom of the hex
+     *  xxxxxx
+     *   xxxx
+     *
+     * @param s size of the hexagon
+     * @param i row num of the hexagon, where i = 0 is the bottom
+     * @return
+     */
+    public static int hexRowOffset(int s, int i) {
+        int effectiveI = i;
+        if(i >= s){
+            effectiveI = s * 2 - i - 1;
+        }
+        return -effectiveI;
+    }
+
+    /** Adds a row of the same tile.
+     * @param world the world to draw on
+     * @param p the leftmost position of the row
+     * @param width the number of tiles wide to draw
+     * @param t the tile to draw
+     */
+    public static void addRow(TETile[][] world, Position p, int width, TETile t) {
+        int y = p.y;
+        for(int x = 0; x < width; x++) {
+            world[p.x + x][y] = TETile.colorVariant(t, 32,32,32, RANDOM);
+        }
+    }
+    /**
+     * Adds a hexagon to the world.
+     * @param world the world to draw on
+     * @param p the bottom left coordinate of the hexagon
+     * @param s the size of the hexagon
+     * @param t the tile to draw
+     */
+    public static void addHexagon(TETile[][] world, Position p, int s, TETile t) {
+        if(s < 2){
+            throw new IllegalArgumentException("Hexagon must be at least 2 tiles");
+        }
+
+        for(int r = 0; r < s * 2; r++) {
+            int xOffset = hexRowOffset(s, r);
+            Position currentP = new Position(p.x + xOffset, p.y + r);
+
+            int width = hexRowWidth(s, r);
+            addRow(world, currentP, width, t);
+        }
+    }
+
+    @Test
+    public void testHexRowWidth() {
+        assertEquals(3, hexRowWidth(3, 5));
+        assertEquals(5, hexRowWidth(3, 4));
+        assertEquals(7, hexRowWidth(3, 3));
+        assertEquals(7, hexRowWidth(3, 2));
+        assertEquals(5, hexRowWidth(3, 1));
+        assertEquals(3, hexRowWidth(3, 0));
+        assertEquals(2, hexRowWidth(2, 0));
+        assertEquals(4, hexRowWidth(2, 1));
+        assertEquals(4, hexRowWidth(2, 2));
+        assertEquals(2, hexRowWidth(2, 3));
+    }
+
+    @Test
+    public void testHexRowOffset() {
+        assertEquals(0, hexRowOffset(3, 5));
+        assertEquals(-1, hexRowOffset(3, 4));
+        assertEquals(-2, hexRowOffset(3, 3));
+        assertEquals(-2, hexRowOffset(3, 2));
+        assertEquals(-1, hexRowOffset(3, 1));
+        assertEquals(0, hexRowOffset(3, 0));
+        assertEquals(0, hexRowOffset(2, 0));
+        assertEquals(-1, hexRowOffset(2, 1));
+        assertEquals(-1, hexRowOffset(2, 2));
+        assertEquals(0, hexRowOffset(2, 3));
+    }
+
+    public static void main(String[] args) {
+        int width = 10, height = 10;
+
+        TERenderer ter = new TERenderer();
+        ter.initialize(width, height);
+
+        TETile[][] randomTiles = new TETile[width][height];
+        //初始化
+        for (int x = 0; x < width; x += 1) {
+            for (int y = 0; y < height; y += 1) {
+                randomTiles[x][y] = Tileset.NOTHING;
+            }
+        }
+        addHexagon(randomTiles, new Position(3,3), 3, Tileset.FLOWER);
+
+        ter.renderFrame(randomTiles);
+        ter.renderFrame(randomTiles);
+    }
 }
