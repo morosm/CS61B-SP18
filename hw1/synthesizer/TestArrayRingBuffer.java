60528da0cc1e98dbc10bb140c63a615fb43fafb4
moros <plumm@foxmail.com>
2024-05-05 12:41:04 +0800

    hw1 finishTask3

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,19 +1,36 @@
 package synthesizer;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/** Tests the ArrayRingBuffer class.
- *  @author Josh Hug
- */
-
-public class TestArrayRingBuffer {
+public class TestArrayRingBuffer
+{
     @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
-    }
+    public void testEnque(){
+        BoundedQueue<Double> x = new ArrayRingBuffer<>(4);
+        assertTrue(x.capacity() == 4);
+        assertTrue(x.isEmpty());
+        assertFalse(x.isFull());
+        assertTrue(x.fillCount() == 0);
+        assertTrue(x.peek() == null);
+        //assertThrow需要4.13才能用
 
-    /** Calls tests for ArrayRingBuffer. */
-    public static void main(String[] args) {
-        jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
+        x.enqueue(33.1); // 33.1 null null  null
+        assertTrue(x.fillCount() == 1);
+        assertTrue(x.peek() == 33.1);
+
+        x.enqueue(44.8); // 33.1 44.8 null  null
+        x.enqueue(62.3); // 33.1 44.8 62.3  null
+        x.enqueue(-3.4); // 33.1 44.8 62.3 -3.4
+        assertTrue(x.fillCount() == 4);
+        assertTrue(x.isFull());
+        assertFalse(x.isEmpty());
+
+        assertTrue(x.dequeue() == 33.1);
+        x.dequeue();
+        x.dequeue();
+        x.dequeue();
+        assertTrue(x.isEmpty());
+        assertFalse(x.isFull());
     }
-} 
+}
