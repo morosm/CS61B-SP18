93c96473a141fd17620317ee0f1cfe52b4d96811
morosm <plumm@foxmail.com>
2024-07-13 18:16:13 +0800

    lab10 fix. size--传入之后就减小了,所以验证bound就失败了

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -177,8 +177,9 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
 
         T value = peek();
 
-        swap(1, size--);
         if(size > 1){
+            swap(1, size);
+            size--;
             sink(1);
             contents[size + 1] = null;
         }else {
